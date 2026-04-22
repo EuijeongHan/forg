29875ac6334d7c1b32b740ca7481c82980466aef
EuijeongHan <hej4016@gmail.com>
2026-04-22 16:32:10 +0900

    fix: use os.environ for DATABASE_URL

--- app/database.py
+++ app/database.py
@@ -1,6 +1,8 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
-from config import DATABASE_URL
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
